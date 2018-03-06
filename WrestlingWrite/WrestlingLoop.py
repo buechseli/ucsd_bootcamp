e94704c03e2cbedaa2162a09b97bbc01c629c0e3
buechseli <34114320+buechseli@users.noreply.github.com>
2018-03-05 21:40:18 -0800

    Work Monday March 5, 2018
    Work Monday March 5, 2018

---
+++ WrestlingWrite/WrestlingLoop.py
@@ -0,0 +1,16 @@
+import os
+
+import csv
+
+
+
+wr_2013=  os.path.join("raw_data","WWE-Data-2013.csv")
+wr_2014=  os.path.join("raw_data","WWE-Data-2014.csv")
+wr_2015=  os.path.join("raw_data","WWE-Data-2015.csv")
+wr_2016=  os.path.join("raw_data","WWE-Data-2016.csv")
+wr_2017=  os.path.join("raw_data","WWE-Data-2017.csv")
+print (wr_2013)
+print (wr_2014)
+print (wr_2015)
+print (wr_2016)
+print (wr_2017)
